39ec8d5943ab7a0d2bed87693061755307ba79c9
protushar <tushar.kr0412@gmail.com>
2019-12-10 21:36:26 +0530

    First Commit

---
+++ src/main/java/com/testing/tusharSample/SampleAdd.java
@@ -0,0 +1,12 @@
+package com.testing.tusharSample;
+
+public class SampleAdd {
+	public static void main(String ar[])
+	{
+		int a=7;
+		int b=8;
+		int sum=a+b;
+		System.out.println(sum);
+	}
+
+}
---
+++ src/test/java/com/testing/tusharSample/TestSample.java
@@ -0,0 +1,20 @@
+package com.testing.tusharSample;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSample {
+
+	@Test
+	public void test()
+	{
+		System.out.println("Succeessful sample run tushar");
+		Assert.assertFalse(false);
+	}
+	@Test
+	public void test1()
+	{
+		System.out.println("Succeessful sample run tushar");
+		Assert.assertFalse(false);
+	}
+}
